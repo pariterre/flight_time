0989c748b67ac9f93cbb61a50e4762da223fd38f
Pariterre <pariterre@hotmail.com>
2024-10-26 19:32:16 -0400

    Starting to populate athletes navigation

--- lib/screens/athletes_navigation_page.dart
+++ lib/screens/athletes_navigation_page.dart
@@ -1,3 +1,5 @@
+import 'package:flight_time/models/athletes.dart';
+import 'package:flight_time/widgets/animated_expanding_card.dart';
 import 'package:flight_time/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -8,11 +10,31 @@ class AthletesNavigationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final names = Athletes.instance.athleteNames;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Athletes'),
-      ),
-      drawer: MainDrawer(),
-    );
+        appBar: AppBar(
+          title: const Text('Athletes'),
+        ),
+        drawer: MainDrawer(),
+        body: SizedBox(
+          child: ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              return AnimatedExpandingCard(
+                header: ListTile(title: Text(names[index])),
+                child: SizedBox(
+                  height: 60 * 3,
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return ListTile(title: Text('Coucou$index'));
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
